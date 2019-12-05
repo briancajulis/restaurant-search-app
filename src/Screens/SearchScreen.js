70d59d9e0191b9684516b7f881e77669cef6c3fe
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2019-12-05 14:08:29 -0800

    Added a call to yelp api

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../Components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]); // expected to be an array of objects
     // Two options to call an api: fetch or axios
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: { // automatically added into the URL
+                limit: 50,
+                location: 'San Jose',
+                term
+            }
+        });
+        setResults(response.data.businesses);
+    }
 
     return (
         <View>
             <SearchBar
                 term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi}
             />
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results.</Text>
         </View>
     );
 }
